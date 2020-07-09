188905f40041f078eb348074af8ae8be48b1cfc1
Mator <why101@msn.com>
2020-07-09 11:33:53 -0700

    added iohelpers

---
+++ esper/helpers/IOHelpers.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace esper.helpers {
+    public static class IOHelpers {
+        public static string ResolveResourcePath(string filename) {
+            return Path.Join(Environment.CurrentDirectory, "data", filename);
+        }
+
+        public static JObject LoadResource(string filename) {
+            var filePath = ResolveResourcePath(filename);
+            return JObject.Parse(File.ReadAllText(filePath));
+        }
+    }
+}
