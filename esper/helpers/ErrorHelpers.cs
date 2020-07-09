248ae6cee9666a72d147b7a0bb6b6f4e929e5658
Mator <why101@msn.com>
2020-07-09 11:33:47 -0700

    added error helpers

---
+++ esper/helpers/ErrorHelpers.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace esper.helpers {
+    public static class ErrorHelpers {
+        public static void CheckDefProperty(JObject src, string property) {
+            if (src.ContainsKey(property)) return;
+            throw new System.Exception("Expected def property 'element'");
+        }
+    }
+}
