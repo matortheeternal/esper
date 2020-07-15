fc2500f884b52fff84e9e2c14fff61df0d0cc17e
Mator <why101@msn.com>
2020-07-14 20:26:12 -0700

    RecordMap implementation, added some more RecordManager extensions

--- esper/plugins/RecordManager.cs
+++ esper/plugins/RecordManager.cs
@@ -1,23 +1,44 @@
 ﻿using esper.elements;
+using System;
 
 namespace esper.plugins {
     public interface IRecordManager {
-        internal RecordMap<ulong> localRecordsByFormID { get; set; }
+        internal RecordMap<ulong> localRecordsByFormId { get; set; }
         internal PluginRecordMap<ulong> remoteRecordsByFormId { get; set; }
         internal PluginFile file { get; }
     }
 
     public static class RecordManagerExtensions {
-        public static MainRecord GetRecordByFormId(this IRecordManager m, ulong formId) {
+        public static void InitRecordMaps(this IRecordManager m) {
+            Func<MainRecord, ulong> GetLocalFormId = rec => rec.localFormId;
+            m.localRecordsByFormId = new RecordMap<ulong>(GetLocalFormId);
+            m.remoteRecordsByFormId = new PluginRecordMap<ulong>(GetLocalFormId);
+        }
+
+        public static MainRecord GetRecordByFormId(
+            this IRecordManager m, ulong formId
+        ) {
             byte ordinal = (byte) (formId >> 24);
             PluginFile targetFile = m.file.OrdinalToFile(ordinal, true);
-            var map = m.remoteRecordsByFormId[targetFile];
             ulong localFormId = formId & 0xFFFFFF;
+            if (targetFile == m.file) 
+                return m.GetLocalRecordByFormId(localFormId);
+            var map = m.remoteRecordsByFormId[targetFile];
             return map[localFormId];
         }
 
-        public static MainRecord GetLocalRecordByFormId(this IRecordManager m, ulong localFormId) {
-            return m.localRecordsByFormID[localFormId];
+        public static MainRecord GetLocalRecordByFormId(
+            this IRecordManager m, ulong localFormId
+        ) {
+            return m.localRecordsByFormId[localFormId];
+        }
+
+        public static void IndexRecord(this IRecordManager m, MainRecord rec) {
+            if (rec.IsLocal()) {
+                m.localRecordsByFormId.Add(rec);
+            } else {
+                m.remoteRecordsByFormId.Add(rec);
+            }
         }
     }
 }
--- esper/plugins/RecordMap.cs
+++ esper/plugins/RecordMap.cs
@@ -1,8 +1,34 @@
 ﻿using esper.elements;
+using System;
 using System.Collections.Generic;
 
 namespace esper.plugins {
-    public class RecordMap<T> : Dictionary<T, MainRecord> {}
+    public class RecordMap<T> : Dictionary<T, MainRecord> {
+        readonly Func<MainRecord, T> GetKey;
 
-    public class PluginRecordMap<T> : Dictionary<PluginFile, RecordMap<T>> {}
+        public RecordMap(Func<MainRecord, T> GetKey) {
+            this.GetKey = GetKey;
+        }
+
+        public void Add(MainRecord record) {
+            this[GetKey(record)] = record;
+        }
+    }
+
+    public class PluginRecordMap<T> : Dictionary<PluginFile, RecordMap<T>> {
+        readonly Func<MainRecord, T> GetKey;
+
+        public PluginRecordMap(Func<MainRecord, T> GetKey) {
+            this.GetKey = GetKey;
+        }
+
+        public void AddMap(PluginFile file) {
+            this[file] = new RecordMap<T>(GetKey);
+        }
+
+        public void Add(MainRecord record) {
+            if (!ContainsKey(record.file)) AddMap(record.file);
+            this[record.file].Add(record);
+        }
+    }
 }
