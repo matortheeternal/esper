4763f07f737967a55f09f559778b19a773956d96
Mator <why101@msn.com>
2020-08-07 16:25:20 -0700

    added AllRecords test (plugin isn't all records yet)

---
+++ Tests/AllRecordsTest.cs
@@ -0,0 +1,27 @@
+﻿using esper;
+using esper.plugins;
+using esper.setup;
+using NUnit.Framework;
+
+namespace Tests {
+    public class AllRecordsTest {
+        public Session session;
+        public PluginManager pluginManager => session.pluginManager;
+        public PluginFile plugin;
+
+        [OneTimeSetUp]
+        public void SetUp() {
+            session = new Session(Games.TES5, new SessionOptions {
+                buildDefsOnDemand = true
+            });
+            var pluginPath = TestHelpers.FixturePath("AllRecords.esp");
+            plugin = pluginManager.LoadPlugin(pluginPath);
+            Assert.IsNotNull(plugin);
+        }
+
+        [Test]
+        public void TestGroups() {
+            Assert.AreEqual(57, plugin.elements.Count);
+        }
+    }
+}
