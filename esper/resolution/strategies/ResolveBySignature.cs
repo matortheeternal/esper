3ac6f58d532db0eb120fee403aefdb504d6a415f
Mator <why101@msn.com>
2020-08-07 12:30:05 -0700

    fixed ResolveBySignature bug

--- esper/resolution/strategies/ResolveBySignature.cs
+++ esper/resolution/strategies/ResolveBySignature.cs
@@ -9,7 +9,7 @@ namespace esper.resolution.strategies {
         public static MatchData Match(Element element, string pathPart) {
             var c = ContainerMatch.From(element, pathPart, signatureExpr);
             if (c == null) return null;
-            var sig = c.match.Groups[0].Value;
+            var sig = c.match.Groups[1].Value;
             return element.SupportsSignature(sig) ? c : null;
         }
 
@@ -21,7 +21,7 @@ namespace esper.resolution.strategies {
 
         public static Element Resolve(MatchData match) {
             ContainerMatch c = (ContainerMatch)match;
-            var sig = c.match.Groups[0].Value;
+            var sig = c.match.Groups[1].Value;
             foreach (Element element in c.container.elements) {
                 if (GroupMatches(element, sig)) return element;
                 if (element.signature == sig) return element;
