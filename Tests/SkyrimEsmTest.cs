d59ab81d33ef6752e2bd96ff7486f14404e40e25
Mator <why101@msn.com>
2020-08-10 00:32:06 -0700

    added more skyrim.esm tests

--- Tests/SkyrimEsmTest.cs
+++ Tests/SkyrimEsmTest.cs
@@ -2,6 +2,8 @@
 using esper.plugins;
 using esper.setup;
 using NUnit.Framework;
+using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace Tests {
@@ -10,6 +12,7 @@ namespace Tests {
         public PluginManager pluginManager => session.pluginManager;
         public string pluginPath;
         public PluginFile plugin;
+        public Stopwatch watch = new Stopwatch();
 
         [OneTimeSetUp]
         public void SetUp() {
@@ -20,12 +23,30 @@ namespace Tests {
             Assert.IsTrue(File.Exists(pluginPath),
                 "Put Skyrim.esm in the fixtures directory to run these tests."
             );
+            watch.Start();
+            plugin = pluginManager.LoadPlugin(pluginPath);
+            watch.Stop();
         }
 
         [Test]
-        public void TestLoad() {
-            plugin = pluginManager.LoadPlugin(pluginPath);
+        public void TestLoadTime() {
             Assert.IsNotNull(plugin);
+            float seconds = watch.ElapsedMilliseconds / 1000.0f;
+            Console.WriteLine($"Loaded Skyrim.esm in {seconds:N2}s");
+            Assert.IsTrue(
+                seconds < 1.5f,
+                "Expected Skyrim.esm to load in under a 1.5 seconds."
+            );
+        }
+
+        [Test]
+        public void TestRecordMap() {
+            var rec = plugin.GetRecordByFormId(0x012E46);
+            Assert.IsNotNull(rec);
+            Assert.AreEqual("ArmorIronGauntlets", rec.editorId);
+            rec = plugin.GetRecordByFormId(0xF);
+            Assert.IsNotNull(rec);
+            Assert.AreEqual("Gold001", rec.editorId);
         }
     }
 }
