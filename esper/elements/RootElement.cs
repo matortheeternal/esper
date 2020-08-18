27acd6ef37719dc3e37993e70ec1f1b14b8a2d33
Mator <why101@msn.com>
2020-08-18 11:41:31 -0700

    added RootElement

---
+++ esper/elements/RootElement.cs
@@ -0,0 +1,15 @@
+﻿using esper.setup;
+
+namespace esper.elements {
+    public class RootElement : Container {
+        private readonly Session session;
+
+        public override DefinitionManager manager => session.definitionManager;
+        public override string name => "Root";
+        public override string displayName => name;
+
+        public RootElement(Session session) : base(null, null) {
+            this.session = session;
+        }
+    }
+}
