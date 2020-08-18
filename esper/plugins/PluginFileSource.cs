b5e9668a7c10383b0477ff91ff1172c71f5f6f48
Mator <why101@msn.com>
2020-08-18 11:49:02 -0700

    subrecord parsing adjustments

--- esper/plugins/PluginFileSource.cs
+++ esper/plugins/PluginFileSource.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.IO.MemoryMappedFiles;
 using System.Text;
 using Ionic.Zlib;
+using esper.defs;
 
 namespace esper.plugins {
     public class PluginFileSource {
@@ -17,9 +18,14 @@ namespace esper.plugins {
         private BinaryReader decompressedReader;
         private UInt32 decompressedDataSize;
         private readonly FileInfo fileInfo;
+        private Subrecord? _currentSubrecord;
 
         public readonly PluginFile plugin;
         public string filePath;
+        public Subrecord currentSubrecord => (Subrecord) _currentSubrecord;
+        private long subrecordEndPos;
+        private long endDataOffset;
+
         public long fileSize => fileInfo.Length;
 
         public bool usingDecompressedStream => decompressedStream != null;
@@ -61,7 +67,6 @@ namespace esper.plugins {
 
         internal void ReadFileHeader(PluginFile file) {
             file.header = MainRecord.Read(file, this, TES4);
-            file.header.ReadElements(this);
         }
 
         internal LocalizedString ReadLocalizedString() {
@@ -70,8 +75,20 @@ namespace esper.plugins {
         }
 
         internal string ReadString() {
-            // TODO: read until null terminator
-            return "";
+            byte[] bytes = new byte[32];
+            int i = 0;
+            do {
+                byte b = reader.ReadByte();
+                if (b == 0) break;
+                if (i > bytes.Length) {
+                    var newBytes = new byte[bytes.Length * 2];
+                    bytes.CopyTo(newBytes, 0);
+                    bytes = newBytes;
+                }
+                bytes[i++] = b;
+            } while (true);
+            if (i == 0) return string.Empty;
+            return stringEncoding.GetString(bytes);
         }
 
         internal string ReadString(int? size) {
@@ -94,6 +111,15 @@ namespace esper.plugins {
                 throw new Exception("Critical error parsing file, read past end offset.");
         }
 
+        internal void StartRead(UInt32 dataSize) {
+            endDataOffset = stream.Position + dataSize;
+        }
+
+        internal void EndRead() {
+            if (stream.Position > endDataOffset)
+                throw new Exception("Critical error parsing file, read past end offset.");
+        }
+
         internal void SetDecompressedStream(byte[] decompressedData) {
             if (decompressedData == null) return;
             decompressedStream = new MemoryStream(decompressedData);
@@ -103,6 +129,40 @@ namespace esper.plugins {
         internal void DiscardDecompressedStream() {
             decompressedStream = null;
             decompressedReader = null;
+        }
+
+        internal void ReadSubrecord() {
+            var subrecord = new Subrecord(this);
+            subrecordEndPos = stream.Position + subrecord.dataSize;
+            _currentSubrecord = subrecord;
+        }
+
+        internal bool NextSubrecord() {
+            if (stream.Position >= endDataOffset) return false;
+            if (_currentSubrecord != null) return true;
+            ReadSubrecord();
+            return true;
+        }
+
+        internal void SubrecordHandled() {
+            if (stream.Position > subrecordEndPos)
+                throw new Exception("Critical error reading subrecord, read past end offset.");
+            if (stream.Position < subrecordEndPos) {
+                // Warn($"Warning: {subrecordEndPos - stream.Position} unread bytes on " +
+                //      $"subrecord {currentSubrecord.signature}");
+                stream.Position = subrecordEndPos;
+            }
+            _currentSubrecord = null;
+        }
+
+        internal void WithRecordData(MainRecord rec, Action action) {
+            if (rec.compressed && !rec.Decompress(this))
+                throw new Exception("Failed to decompress content.");
+            try {
+                action();
+            } finally {
+                DiscardDecompressedStream();
+            }
         }
     }
 }
