d5737801b8946b789b935e19a71e256d43936135
Mator <why101@msn.com>
2020-08-19 22:21:07 -0700

    updated CTDATypeFormat to use CtdaTypeEnum from defs

--- esper/defs/TES5/Formats/CTDATypeFormat.cs
+++ esper/defs/TES5/Formats/CTDATypeFormat.cs
@@ -8,23 +8,18 @@ namespace esper.defs.TES5 {
         public static string defType = "CtdaTypeFormat";
 
         private static FlagsDef ctdaTypeFlags;
+        private static EnumDef ctdaTypeEnum;
 
         public CTDATypeFormat(DefinitionManager manager, JObject src)
             : base(manager, src) {
             var flagsSrc = manager.ResolveDefSource("CtdaTypeFlags");
             ctdaTypeFlags = (FlagsDef) manager.BuildDef(flagsSrc);
+            var enumSrc = manager.ResolveDefSource("CtdaTypeEnum");
+            ctdaTypeEnum = (EnumDef) manager.BuildDef(enumSrc);
         }
 
         public override string DataToValue(ValueElement element, dynamic data) {
-            string type = (data & 0xE0) switch {
-                0x00 => "Equal to",
-                0x20 => "Not equal to",
-                0x40 => "Greater than",
-                0x60 => "Greater than or equal to",
-                0x80 => "Less than",
-                0xA0 => "Less than or equal to",
-                _ => "<Unknown Compare operator>"
-            };
+            string type = ctdaTypeEnum.DataToValue(element, data & 0xE0);
             string flags = ctdaTypeFlags.DataToValue(element, data & 0x1F);
             return flags != "" ? $"{type} / {flags}" : type;
         }
