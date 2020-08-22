555859e24fee192d2750ccd88aa21d5447f34e59
Mator <why101@msn.com>
2020-08-22 14:52:10 -0700

    simplified NVNM parent decider a bit

--- esper/defs/TES5/Deciders/NVNMParentDecider.cs
+++ esper/defs/TES5/Deciders/NVNMParentDecider.cs
@@ -5,12 +5,7 @@ using System;
 namespace esper.defs.TES5 {
     public class NVNMParentDecider : Decider {
         public override int Decide(Container container) {
-            // TODO: make this better
-            GroupRecord group = (GroupRecord) container.GetParentElement(
-                e => e is GroupRecord
-            );
-            if (group == null) return 0;
-            var rec = group.GetParentRecord();
+            var rec = container?.group?.GetParentRecord();
             if (rec == null) return 0; // TODO?
             if (rec.signature != "CELL") 
                 throw new Exception("Parent of a NVNM is not a CELL");
