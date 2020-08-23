83617c2702e509df5a182440cbbba079f62b69e7
Mator <why101@msn.com>
2020-08-23 10:35:43 -0700

    added ElementWarnings class

---
+++ esper/warnings/ElementWarnings.cs
@@ -0,0 +1,13 @@
+﻿using esper.elements;
+using System.Collections.Generic;
+
+namespace esper.warnings {
+    public class ElementWarnings : List<ElementWarning> {
+        public bool Add(Element element, string warning) {
+            Add(new ElementWarning(element) {
+                warning = warning
+            });
+            return true;
+        }
+    }
+}
