e6e6b110aed28ec4cefa30d0f2a5972889f3cc27
Mator <why101@msn.com>
2022-01-07 14:40:01 -0800

    implement ConflictCell class

---
+++ esper/conflicts/ConflictCell.cs
@@ -0,0 +1,15 @@
+﻿using esper.elements;
+
+namespace esper.conflicts {
+    public class ConflictCell {
+        public Element element;
+        public CellConflictStatus conflictStatus;
+
+        public string value => element != null ? element.sortKey : "";
+        public bool ignored => element?.def.conflictType != defs.ConflictType.Ignore;
+
+        public ConflictCell(Element element) {
+            this.element = element;
+        }
+    }
+}
