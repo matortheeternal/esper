8f61957e9f3376d47b95db9b84b371ad0d1d1f86
Mator <why101@msn.com>
2022-01-07 14:40:32 -0800

    implement ConflictView class

---
+++ esper/conflicts/ConflictView.cs
@@ -0,0 +1,14 @@
+﻿using esper.elements;
+
+namespace esper.conflicts {
+    public class ConflictView {
+        public ConflictRow row;
+
+        public ConflictView(MainRecord rec) {
+            var master = rec.master;
+            var records = master.overrides;
+            records.Insert(0, master);
+            row = new ConflictRow(records);
+        }
+    }
+}
