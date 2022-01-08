c175b2c6977ad116d26e485bae9386f41e7e169b
Mator <why101@msn.com>
2022-01-07 16:14:45 -0800

    implement child row loading

--- esper/conflicts/ConflictRow.cs
+++ esper/conflicts/ConflictRow.cs
@@ -1,5 +1,7 @@
-﻿using esper.elements;
+﻿using esper.defs;
+using esper.elements;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace esper.conflicts {
     public class ConflictRow {
@@ -8,9 +10,8 @@ namespace esper.conflicts {
         public List<ConflictCell> cells;
 
         public ConflictRow(List<Element> elements) {
-            cells = new List<ConflictCell>(elements.Count);
-            childRows = new List<ConflictRow>(elements.Count);
             InitCells(elements);
+            LoadChildRows(elements);
             CalculateConflicts(elements);
         }
 
@@ -24,9 +25,46 @@ namespace esper.conflicts {
         }
 
         private void InitCells(List<Element> elements) {
+            cells = new List<ConflictCell>(elements.Count);
             foreach (Element element in elements) {
                 var cell = new ConflictCell(element);
                 cells.Add(cell);
+            }
+        }
+
+        private void AppendUnsortedRow(
+            int elementCount, List<int> counts, 
+            List<Container> containers, int elementIndex
+        ) {
+            var childElements = new List<Element>(elementCount);
+            for (int i = 0; i < elementCount; i++) {
+                childElements.Add(elementIndex < counts[i]
+                    ? containers[i].elements[elementIndex]
+                    : null);
+            }
+            childRows.Add(new ConflictRow(childElements));
+        }
+
+        private void AddUnsortedChildRows(List<Element> elements) {
+            var elementCount = elements.Count;
+            var containers = elements.Select(e => e as Container).ToList();
+            var counts = containers.Select(c => c != null ? c.count : 0).ToList();
+            for (int i = 0; i < counts.Max(); i++)
+                AppendUnsortedRow(elementCount, counts, containers, i);
+        }
+
+        private void AddSortedChildRows(List<Element> elements) {
+            throw new System.NotImplementedException();
+        }
+
+        private void LoadChildRows(List<Element> elements) {
+            var firstElement = elements.First(e => e != null);
+            if (!(firstElement is Container container)) return;
+            childRows = new List<ConflictRow>();
+            if (firstElement.def is ArrayDef aDef && aDef.sorted) {
+                AddSortedChildRows(elements);
+            } else {
+                AddUnsortedChildRows(elements);
             }
         }
     }
