deab5d6993d0f66d0c6b73d1b3bcd06e3d6af5e7
Mator <why101@msn.com>
2022-01-23 17:39:11 -0800

    add Benchmarks project

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using esper;
+using esper.plugins;
+using esper.setup;
+using esper.resolution;
+
+namespace Benchmarks {
+    class Program {
+        public Session session;
+        public PluginManager pluginManager => session.pluginManager;
+        public ResourceManager resourceManager;
+        public PluginFile plugin;
+
+        public static string fixturesPath = Path.Combine(
+            Environment.CurrentDirectory,
+            "fixtures"
+        );
+
+        public static string FixturePath(string filename) {
+            return Path.Combine(fixturesPath, filename);
+        }
+
+        private List<string> GetStringFiles() {
+            var skyrimStringsPath = FixturePath("skyrim_strings");
+            var stringFiles = Directory.GetFiles(
+                skyrimStringsPath, "skyrim_english.*"
+            ).ToList();
+            return stringFiles;
+        }
+
+        private void LoadSkyrimEsm() {
+            var pluginPath = FixturePath("Skyrim.esm");
+            plugin = pluginManager.LoadPlugin(pluginPath);
+        }
+
+        public void SetUp() {
+            session = new Session(Games.TES5, new SessionOptions());
+            resourceManager = new ResourceManager(session, true);
+            LoadSkyrimEsm();
+            resourceManager.LoadPluginStrings(plugin, GetStringFiles());
+        }
+
+        public void TouchSubrecords(string signature, string path) {
+            var records = plugin.GetElements(signature);
+            foreach (var record in records)
+                record.GetValue(path);
+        }
+
+        static void Main(string[] args) {
+            var p = new Program();
+            p.SetUp();
+            p.TouchSubrecords("WEAP", "FULL");
+        }
+    }
+}
