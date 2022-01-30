b94aa4ec94f6060b26038abe37ffd00f2a4b627d
Mator <why101@msn.com>
2022-01-30 13:30:47 -0800

    minor refactor - loading definitions from zip working

--- esper/helpers/IOHelpers.cs
+++ esper/helpers/IOHelpers.cs
@@ -1,17 +1,22 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
 using System.IO.Compression;
 
 namespace esper.helpers {
     public static class IOHelpers {
-        public static JObject LoadDefinitions(string filename) {
+        private static ZipArchive LoadDefinitionsArchive() {
             var definitionsPath = Path.Join(Environment.CurrentDirectory, "definitions.zip");
             var stream = File.OpenRead(definitionsPath);
-            var archive = new ZipArchive(stream, ZipArchiveMode.Read);
+            return new ZipArchive(stream, ZipArchiveMode.Read);
+        }
+
+        internal static JObject LoadDefinitions(string filename) {
+            var archive = LoadDefinitionsArchive();
             var entry = archive.GetEntry(filename);
             var reader = new StreamReader(entry.Open());
-            return JObject.Parse(reader.ReadToEnd());
+            return JObject.Load(new JsonTextReader(reader));
         }
     }
 }
