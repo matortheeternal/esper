ddb3764a425f7d466a13200e0470fe9c42e79a60
Mator <why101@msn.com>
2022-02-03 12:18:22 -0800

    temporarily disabled OffsetDataColsCounter

--- esper/defs/TES5/Counters/OffsetDataColsCounter.cs
+++ esper/defs/TES5/Counters/OffsetDataColsCounter.cs
@@ -13,14 +13,15 @@ namespace esper.defs.TES5 {
         ) : base(manager, src) {}
 
         public override UInt32 GetCount(Container container) {
-            var bounds = container.GetElement(@"..\Object Bounds");
+            return 0;
+            /*var bounds = container.GetElement(@"..\Object Bounds");
             float? minX = bounds?.GetData(@"NAM0\X");
             float? maxX = bounds?.GetData(@"NAM9\X");
             if (minX == null || maxX == null) return 0;
             if (minX >= int.MaxValue || minX <= 0) return 0;
             var max = (int.MaxValue - minX + 1);
             if (maxX >= max || maxX <= 1) return 1;
-            return (UInt32) (Math.Truncate((float) maxX) - minX + 1);
+            return (UInt32) (Math.Truncate((float) maxX) - minX + 1);*/
         }
     }
 }
